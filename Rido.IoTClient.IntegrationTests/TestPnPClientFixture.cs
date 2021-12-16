55b91862c4d2c97139b2c407c307dbc2be88c298
rido-min <rido-min@users.noreply.github.com>
2021-12-16 09:32:07 -0800

    d10

--- Rido.IoTClient.IntegrationTests/TestPnPClientFixture.cs
+++ Rido.IoTClient.IntegrationTests/TestPnPClientFixture.cs
@@ -10,51 +10,26 @@ using Rido.IoTClient;
 
 namespace Rido.IoTClient.IntegrationTests
 {
-    public class TestPnPClientFixture : IDisposable
+    public class TestPnPClientFixture 
     {
-        TestPnPClient? client = null;
-        private bool disposedValue;
-        ConnectionSettings? cs = null;
-        public TestPnPClientFixture()
+        ConnectionSettings cs = new ConnectionSettings()
         {
-            cs = new ConnectionSettings()
-            {
-                HostName = "tests.azure-devices.net",
-                DeviceId = "d5",
-                SharedAccessKey = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.Empty.ToString("N")))
-            };
-        }
-
+            HostName = "tests.azure-devices.net",
+            DeviceId = "d10",
+            SharedAccessKey = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.Empty.ToString("N")))
+        };
 
         [Fact]
         public async Task ValidateReadOnlyProperty()
         {
-            client = TestPnPClient.CreateAsync(cs).Result;
+            var client = await TestPnPClient.CreateAsync(cs);
             await client.Property_person.UpdateTwinPropertyAsync(new Person { Name = "rido", Age = 33 });
             var twinJson = await client.GetTwinAsync();
             var twin = JsonNode.Parse(twinJson);
             Assert.NotNull(twin);
             Assert.Equal("rido", twin?["reported"]?[client.Property_person.Name]?["Name"]?.GetValue<string>());
             Assert.Equal("rido", client.Property_person.PropertyValue.Name);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    client.Connection.Dispose();
-                }
-                disposedValue = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            await client.Connection.DisconnectAsync(MqttClientDisconnectReason.NormalDisconnection);
         }
     }
 }
