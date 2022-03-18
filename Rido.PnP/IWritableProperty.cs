2f928536101fcacc627ff88341995d9f9b3ea9dd
ridomin <ridomin@live.com>
2022-03-18 15:03:15 -0700

    start ref

---
+++ Rido.PnP/IWritableProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rido.PnP
+{
+    public interface IWritableProperty<T>
+    {
+        PropertyAck<T> PropertyValue { get; set; }
+
+        Func<PropertyAck<T>, Task<PropertyAck<T>>> OnProperty_Updated { get; set; }
+
+        Task InitPropertyAsync(string twin, T defaultValue, CancellationToken cancellationToken = default);
+        Task<int> ReportPropertyAsync(CancellationToken token = default);
+    }
+}
---
+++ Rido.PnP/ReadOnlyProperty.cs
@@ -0,0 +1,53 @@
+﻿using Rido.MqttCore;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rido.PnP
+{
+    public class ReadOnlyProperty<T> : IReadOnlyProperty<T>
+    {
+        private readonly IPropertyStoreWriter updateTwin;
+        public readonly string Name;
+        private readonly string component;
+        public T PropertyValue { get; set; }
+        public int Version { get; set; }
+
+        public ReadOnlyProperty(IMqttBaseClient connection, string name, string component = "")
+        {
+            updateTwin = new UpdateTwinBinder(connection);
+            Name = name;
+            this.component = component;
+        }
+
+        public async Task<int> ReportPropertyAsync(CancellationToken cancellationToken = default)
+        {
+            bool asComponent = !string.IsNullOrEmpty(component);
+            Version = await updateTwin.ReportPropertyAsync(ToJsonDict(asComponent), cancellationToken);
+            return Version;
+        }
+
+        private Dictionary<string, object> ToJsonDict(bool asComponent = false)
+        {
+            Dictionary<string, object> result;
+            if (asComponent == false)
+            {
+                result = new Dictionary<string, object> { { Name, PropertyValue } };
+            }
+            else
+            {
+                Dictionary<string, Dictionary<string, object>> dict = new Dictionary<string, Dictionary<string, object>>
+                {
+                    { component, new Dictionary<string, object>() }
+                };
+                dict[component].Add("__t", "c");
+                dict[component].Add(Name, PropertyValue);
+                result = dict.ToDictionary(pair => pair.Key, pair => (object)pair.Value);
+            }
+            return result;
+        }
+
+    }
+}
