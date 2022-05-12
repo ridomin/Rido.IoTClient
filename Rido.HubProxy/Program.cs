2676dbab246c5cf8938d1c7f37264892a3edc3f4
ridomin <ridomin@live.com>
2022-05-12 13:48:34 -0700

    clean proxy

--- Rido.HubProxy/Program.cs
+++ Rido.HubProxy/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Diagnostics;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Serialization;
-using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using static Rido.HubProxy.Imemmon;
 
@@ -24,11 +22,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//string did = "memmon1";
+var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
 
 app.MapGet("/pnp/{did}/props/started", async (string did) =>
 {
-    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
     var twin = await dtc.GetDigitalTwinAsync<BasicDigitalTwin>(did);
     if (twin.Body.CustomProperties.TryGetValue("started", out object? propValue))
     {
@@ -51,7 +48,6 @@ app.MapGet("/pnp/{did}/props/started", async (string did) =>
 
 app.MapGet("/pnp/{did}/props/interval", async (string did) =>
 {
-    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
     var twin = await dtc.GetDigitalTwinAsync<BasicDigitalTwin>(did);
     if (twin.Body.CustomProperties.TryGetValue("interval", out object? propValue))
     {
@@ -73,7 +69,6 @@ app.MapGet("/pnp/{did}/props/interval", async (string did) =>
 
 app.MapGet("/pnp/{did}/props/enabled", async (string did) =>
 {
-    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
     var twin = await dtc.GetDigitalTwinAsync<BasicDigitalTwin>(did);
     if (twin.Body.CustomProperties.TryGetValue("enabled", out object? propValue))
     {
@@ -97,7 +92,6 @@ app.MapGet("/pnp/{did}/props/enabled", async (string did) =>
 
 app.MapPost("/pnp/{did}/props/interval", async (string did, [FromBody]int propVal) =>
 {
-    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
     var updOp = new UpdateOperationsUtility();
     updOp.AppendAddPropertyOp("/interval", propVal);
     var res = await dtc.UpdateDigitalTwinAsync(did, updOp.Serialize());
@@ -109,7 +103,6 @@ app.MapPost("/pnp/{did}/props/interval", async (string did, [FromBody]int propVa
 
 app.MapPost("/pnp/{did}/props/enabled", async (string did, [FromBody] bool propVal) =>
 {
-    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
     var updOp = new UpdateOperationsUtility();
     updOp.AppendAddPropertyOp("/enabled", propVal);
     var res = await dtc.UpdateDigitalTwinAsync(did, updOp.Serialize());
@@ -120,7 +113,6 @@ app.MapPost("/pnp/{did}/props/enabled", async (string did, [FromBody] bool propV
 
 app.MapPost("/pnp/{did}/commands/getRuntimeStats", async (string did, [FromBody] DiagnosticsMode diagMode) =>
 {
-    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
     var resp = await dtc.InvokeCommandAsync(did, "getRuntimeStats", JsonSerializer.Serialize(diagMode),
         new DigitalTwinInvokeCommandRequestOptions { ConnectTimeoutInSeconds = 3, ResponseTimeoutInSeconds = 5 });
     resp.Response.EnsureSuccessStatusCode();
@@ -129,90 +121,4 @@ app.MapPost("/pnp/{did}/commands/getRuntimeStats", async (string did, [FromBody]
     
 }).WithName("command_getRuntimeStats");
 
-
-
-
-//app.MapGet("/pnp/{did}/props/interval", async (string did) =>
-//{
-//    var dtc = DigitalTwinClient.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
-//    var twin = await dtc.GetDigitalTwinAsync<BasicDigitalTwin>(did);
-//    if (twin.Body.CustomProperties.TryGetValue("interval", out object? rawValue))
-//    {
-//        if (Int32.TryParse(rawValue.ToString(), out int interval))
-//        {
-//            return Results.Ok(interval);
-//        }
-//        else
-//        {
-//            return Results.Problem("Property started is not int", rawValue.ToString(), 501);
-//        }
-//    }
-//    else
-//    {
-//        return Results.NotFound();
-//    }
-
-//}).WithName("readProperty_interval");
-
-
-
-
-//app.MapGet("/interval", async () => 
-//{
-//    var rm = RegistryManager.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
-//    var twin = await rm.GetTwinAsync(did);
-//    var intervalProp = TwinProperty<int>.InitFromTwin(twin.ToJson(), "interval", string.Empty, -1);
-//    app.Logger.LogInformation("twin:" + twin.ToJson());
-//    return intervalProp;
-//}).WithName("readProperty_interval");
-
-//app.MapPost("/interval", async (int interval) =>
-//{
-//    var rm = RegistryManager.CreateFromConnectionString(app.Configuration.GetConnectionString("hub"));
-//    var twin = await rm.GetTwinAsync(did);
-//    var patch = new
-//    {
-//        properties = new
-//        {
-//            desired = new
-//            {
-//                interval
-//            }
-//        }
-//    };
-//    await rm.UpdateTwinAsync(did, JsonSerializer.Serialize(patch), twin.ETag);
-//    await Task.Delay(500);
-//    twin = await rm.GetTwinAsync(did);
-//    var intervalProp = TwinProperty<int>.InitFromTwin(twin.ToJson(), "interval", string.Empty, -1);
-//    app.Logger.LogInformation("twin:" + twin.ToJson());
-//    return intervalProp;
-//}).WithName("writeProperty_interval");
-
-
-
-
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-
 app.Run();
-
-//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
