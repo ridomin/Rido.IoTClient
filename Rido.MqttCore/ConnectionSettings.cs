3668528e97b371c03d67d57a8acadbb48d4f5c83
rido-min <rido-min@users.noreply.github.com>
2022-09-06 10:59:28 -0700

    TSL wip

--- Rido.MqttCore/ConnectionSettings.cs
+++ Rido.MqttCore/ConnectionSettings.cs
@@ -22,6 +22,7 @@ namespace Rido.MqttCore
         private const string Default_CleanSession = "true";
         private const int Default_TcpPort = 8883;
         private const string Default_UseTls = "true";
+        private const string Default_DisableCrl = "false";
 
         public string IdScope { get; set; }
         public string HostName { get; set; }
@@ -39,6 +40,8 @@ namespace Rido.MqttCore
         public bool CleanSession { get; set; }
         public int TcpPort { get; set; }
         public bool UseTls { get; set; }
+        public string CaPath { get; set; }
+        public bool DisableCrl { get; set; }
 
         public ConnectionSettings()
         {
@@ -92,6 +95,8 @@ namespace Rido.MqttCore
             CleanSession = GetStringValue(map, nameof(CleanSession), Default_CleanSession) == "true";
             TcpPort = GetPositiveIntValueOrDefault(map, nameof(TcpPort), Default_TcpPort);
             UseTls = GetStringValue(map, nameof(UseTls), Default_UseTls) == "true";
+            CaPath = GetStringValue(map, nameof(CaPath));
+            DisableCrl = GetStringValue(map, nameof(DisableCrl), Default_DisableCrl) == "true";
             
             //if (string.IsNullOrEmpty(SharedAccessKey) && string.IsNullOrEmpty(X509Key) && string.IsNullOrEmpty(Password))
             //{
---
+++ mqtt-adapters/Rido.Mqtt.MqttNet4Adapter/WithX509Auth.cs
@@ -0,0 +1,76 @@
+﻿using MQTTnet.Client;
+using Rido.MqttCore.Birth;
+using Rido.MqttCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using MQTTnet.Implementations;
+
+namespace Rido.Mqtt.MqttNet4Adapter
+{
+    public static partial class MqttNetExtensions
+    {
+        public static MqttClientOptionsBuilder WithX509Auth(this MqttClientOptionsBuilder builder, ConnectionSettings cs, bool withLWT = false)
+        {
+            var certs = new List<X509Certificate2>();
+
+            if (!string.IsNullOrEmpty(cs.X509Key))
+            {
+                var cert = ClientCertificateLocator.Load(cs.X509Key);
+                certs.Add(cert);
+            }
+
+            if (!string.IsNullOrEmpty(cs.Password))
+            {
+                builder.WithCredentials(cs.UserName, cs.Password);
+            }
+
+            var tls = new MqttClientOptionsBuilderTlsParameters();
+            tls.UseTls = cs.UseTls;
+            tls.IgnoreCertificateRevocationErrors = cs.DisableCrl;
+            if (!string.IsNullOrEmpty(cs.CaPath))
+            {
+                var caCert = new X509Certificate2(cs.CaPath);
+                //certs.Add(caCert);
+                tls.CertificateValidationHandler = ea =>
+                {
+#if NET6_0
+                    X509Chain chain = new X509Chain();
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+                    chain.ChainPolicy.VerificationTime = DateTime.Now;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 0, 0);
+                    chain.ChainPolicy.CustomTrustStore.Add(caCert);
+                    chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+                    var x5092 = new X509Certificate2(ea.Certificate);
+                    return chain.Build(x5092);
+#endif
+#if NETSTANDARD2_0
+                    return ea.Certificate.Issuer == caCert.Subject;
+#endif
+                };
+            }
+            
+            tls.Certificates = certs;
+            builder
+                .WithTcpServer(cs.HostName, cs.TcpPort)
+                .WithClientId(cs.ClientId)
+                .WithKeepAlivePeriod(TimeSpan.FromSeconds(cs.KeepAliveInSeconds))
+                .WithCleanSession(true)
+                .WithTls(tls);
+
+            if (withLWT)
+            {
+                builder
+                .WithWillTopic(BirthConvention.BirthTopic(cs.ClientId))
+                .WithWillQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+                .WithWillPayload(BirthConvention.LastWillPayload(cs.ModelId))
+                .WithWillRetain(true);
+            }
+            return builder;
+        }
+    }
+}
+
